ff61475b6bf68470ad70adff5cd00ded9e525994
Kunimatsu Hashimoto <kunimatsu.hashimoto@tri.global>
2017-12-14 11:41:36 -0500

    Image: Add a member function

--- systems/sensors/image.h
+++ systems/sensors/image.h
@@ -117,6 +117,13 @@ class Image {
     height_ = height;
   }
 
+  /// Fills all the pixels in an image with a given value.
+  ///
+  /// @param value A value to be filled in.
+  void fill(T value) {
+    std::fill(data_.begin(), data_.end(), value);
+  }
+
   /// Access to the pixel located at (x, y) in image coordinate system where x
   /// is the variable for horizontal direction and y is one for vertical
   /// direction.  To access to the each channel value in the pixel (x, y),
